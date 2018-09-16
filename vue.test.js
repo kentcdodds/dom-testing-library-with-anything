28a96695aa3af877b0951cdbbee8fdb4efb5bf1f
Dobromir Hristov <dobri@digital-cult.com>
2018-09-16 17:46:58 +0300

    add vue example (#3)
    * add vue example
    
    * fix: use async events instead of Vue.nextTick

---
+++ vue.test.js
@@ -0,0 +1,37 @@
+import Vue from 'vue/dist/vue'
+import 'jest-dom/extend-expect'
+import {fireEventAsync} from './fire-event-async'
+import {getQueriesForElement} from 'dom-testing-library'
+
+const Counter = {
+  template: `
+    <div>
+        <button @click='increment'>
+            {{count}}
+        </button>
+    </div>`,
+  data: () => ({count: 0}),
+  methods: {
+    increment() {
+      this.count++
+    },
+  },
+}
+
+function render(Component) {
+  const vm = new Vue(Component).$mount()
+  return {
+    container: vm.$el,
+    ...getQueriesForElement(vm.$el),
+  }
+}
+
+test('counter increments', async () => {
+  const {getByText} = render(Counter)
+  const counter = getByText('0')
+  await fireEventAsync.click(counter)
+  expect(counter).toHaveTextContent('1')
+
+  await fireEventAsync.click(counter)
+  expect(counter).toHaveTextContent('2')
+})
