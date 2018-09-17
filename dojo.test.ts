3e1c1d4a7488156c7e4ba7a50770cc550f42b86f
Anthony Gubler <agubler@gmail.com>
2018-09-17 08:05:57 -0700

    Added dojo example (#12)

---
+++ dojo.test.ts
@@ -0,0 +1,39 @@
+import ProjectorMixin from '@dojo/framework/widget-core/mixins/Projector'
+import WidgetBase from '@dojo/framework/widget-core/WidgetBase'
+import {v} from '@dojo/framework/widget-core/d'
+import {Constructor} from '@dojo/framework/widget-core/interfaces'
+import {getQueriesForElement, fireEvent} from 'dom-testing-library'
+import 'jest-dom/extend-expect'
+
+class Counter extends WidgetBase {
+  count = 0
+  increment() {
+    this.count++
+    this.invalidate()
+  }
+  render() {
+    return v('div', [v('button', {onclick: this.increment}, [`${this.count}`])])
+  }
+}
+
+function render(ui: Constructor<WidgetBase>) {
+  const container = document.createElement('div')
+  document.body.appendChild(container)
+  const Projector = ProjectorMixin(ui)
+  const projector = new Projector()
+  projector.async = false
+  projector.append(container)
+  return {
+    container,
+    ...getQueriesForElement(container),
+  }
+}
+
+test('renders counter', () => {
+  const {getByText} = render(Counter)
+  const counter = getByText('0')
+  fireEvent.click(counter)
+  expect(counter).toHaveTextContent('1')
+  fireEvent.click(counter)
+  expect(counter).toHaveTextContent('2')
+})
