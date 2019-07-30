9b9d3c03fd4ffb9d138bf55511cf63c3af357847
Kent C. Dodds <kent@doddsfamily.us>
2019-07-30 15:46:08 -0600

    improve svelte

--- svelte.test.js
+++ svelte.test.js
@@ -1,23 +1,18 @@
 import '@testing-library/jest-dom/extend-expect'
 import {fireEventAsync} from './fire-event-async'
-import {getQueriesForElement, fireEvent, wait} from '@testing-library/dom'
+import {getQueriesForElement} from '@testing-library/dom'
 import * as svelte from 'svelte'
 import Counter from './counter.svelte'
 
 function render(Component) {
   const container = document.createElement('div')
-  document.body.appendChild(container)
 
-  const ComponentConstructor = Component.default || Component
+  const ComponentConstructor = Component
   const component = new ComponentConstructor({target: container})
 
   return {
-    ...getQueriesForElement(document.body),
+    ...getQueriesForElement(container),
     container,
-    cleanup() {
-      component.$destroy()
-      document.body.removeChild(container)
-    },
   }
 }
 
