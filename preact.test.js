b04d9cf61874548c23341160174945939644b513
Nick McCurdy <nick@nickmccurdy.com>
2020-08-27 15:10:45 -0400

    Update Preact example to use Preact X hooks
    This upgrades Preact from 8 to X while making the example more consistent with the migration docs and the existing React hooks example.

--- preact.test.js
+++ preact.test.js
@@ -1,26 +1,25 @@
-/** @jsx Preact.h */
+/** @jsx h */
 import '@testing-library/jest-dom/extend-expect'
-import * as Preact from 'preact'
+import {h, render as preactRender} from 'preact'
+import {useState} from 'preact/hooks'
 import {getQueriesForElement, fireEvent, wait} from '@testing-library/dom'
 
-class Counter extends Preact.Component {
-  state = {count: 0}
-  increment = () => this.setState(({count}) => ({count: count + 1}))
-  render() {
-    return (
-      <div>
-        <button onClick={this.increment}>{this.state.count}</button>
-      </div>
-    )
-  }
+function Counter() {
+  const [count, setCount] = useState(0)
+  const increment = () => setCount(c => c + 1)
+  return (
+    <div>
+      <button onClick={increment}>{count}</button>
+    </div>
+  )
 }
 
 function render(ui) {
   const container = document.createElement('div')
-  Preact.render(ui, container)
+  preactRender(ui, container)
   return {
-    container,
     ...getQueriesForElement(container),
+    container,
   }
 }
 
